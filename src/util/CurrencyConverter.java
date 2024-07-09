41b1bf63f5931654309888cad5d87a252d86ef0b
Lucas Rodriguez Sinni <sinniknot@gmail.com>
2024-07-09 13:48:31 -0300

    Exercício finalizado.
    Seguindo os conceitos de membros estáticos, não foi necessário inicializar um objeto da classe CurrencyConverter, apenas chamar o método da mesma.

--- src/util/CurrencyConverter.java
+++ src/util/CurrencyConverter.java
@@ -2,4 +2,12 @@ package util;
 
 public class CurrencyConverter {
 
+	public static final double IOF = 1.06;
+	// Como o valor da porcentagem de IOF já estava definido para 6%, fazer a
+	// multiplicação por 1.06 é equivalente em aumentar o valor em 6%
+
+	public static double convert(double dollarPrice, double amount) {
+		return amount * dollarPrice * IOF;
+	}
+
 }
